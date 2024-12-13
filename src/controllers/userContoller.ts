a1ab3699c172786504ee7029d2a029e110a82495
asbin007 <asbingamer@gmail.com>
2024-12-13 12:13:25 +0545

    we added login and register api

---
+++ src/controllers/userContoller.ts
@@ -0,0 +1,66 @@
+import { Request,Response } from "express";
+import User from "../models/userModel";
+import sequelize from "../database/connection";
+import bcrypt from 'bcrypt'
+
+class UserController{
+    static async register(req:Request,res:Response){
+        // incomming user data receive
+        const {username,password,email}=req.body;
+        if(!username || !password||!email){
+            res.status(400).json({
+                message:"Please provide usernmae,password and email"
+            })
+        }return;
+
+        // data=--> user table mah insert garne
+        await User.create({
+            username,
+            email,
+            password: bcrypt.hashSync(password,10)
+        })
+        res.status(201).json({
+            message:"User registered succesfully"
+        })
+    }
+
+    static async login(req:Request,res:Response){
+
+        // accept incomming data-> email,password
+        const {email,password}=req.body;
+        if(!email ||! password){
+            res.status(400).json({
+                message: "Please provide email and password"
+            })
+            return
+        }
+        // checking email that exist or not
+// note find-->findALl -- array, findId---> findByPk->object 
+        const [user]= await User.findAll({
+            where:{
+                email:email
+            }
+        })
+
+
+        if(!user){
+            res.status(400).json({
+                message: "No user with this email"
+            })
+        }
+        else{
+            // if exits then 
+            const isEqual=bcrypt.compareSync(password,user.password);
+            if(!isEqual){
+                res.status(400).json({
+                    message:"Invalid password"
+                })
+            }else{
+                res.status(200).json({
+                    message:"Logined Successfully"
+                })
+            }
+        }
+    }
+}
+export default UserController
---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import UserController from '../controllers/userContoller';
+
+const router=express.Router();
+
+
+// router.post("/register",UserController.register)
+// router.get("/register",UserController.register)
+
+router.route('/register').post(UserController.register)
+router.route('/login').post(UserController.login)
+
+export default router
